414047507dba81bd782e1f3076d2d52bdbe187ee
Daniel Avellaneda <davellanedam@gmail.com>
2018-09-20 23:08:08 -0700

    First stable release

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+v.1.0.0 (Sep 20, 2018)
+* First stable release
--- app/models/user.js
+++ app/models/user.js
@@ -26,8 +26,8 @@ const UserSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ['programmer', 'company', 'admin'],
-      default: 'programmer'
+      enum: ['user', 'admin'],
+      default: 'user'
     },
     verification: {
       type: String
