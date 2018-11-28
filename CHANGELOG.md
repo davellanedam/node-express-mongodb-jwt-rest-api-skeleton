146cb40afc43b3f972af3a6f9cc58cff0beb0c18
Daniel Avellaneda <davellanedam@gmail.com>
2018-11-28 10:41:23 -0800

    * FIXED: Password not encrypted when updating in profile (https://github.com/davellanedam/node-express-mongodb-jwt-rest-api-skeleton/issues/3) * NPM updated

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v.1.2.3 (Nov 28, 2018)
+* FIXED: Password not encrypted when updating in profile [\#3](https://github.com/davellanedam/node-express-mongodb-jwt-rest-api-skeleton/issues/3)
+* NPM updated
+
 ## v.1.2.2 (Nov 8, 2018)
 * NPM updated
 
--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -10,24 +10,34 @@ const updateProfileInDB = async (req, id) => {
     delete req.body._id
     delete req.body.role
     delete req.body.email
-    model.findByIdAndUpdate(
-      id,
-      req.body,
-      {
-        new: true,
-        runValidators: true,
-        select: '-role -_id -updatedAt -createdAt'
-      },
-      (err, user) => {
-        if (err) {
-          reject(buildErrObject(422, err.message))
-        }
-        if (!user) {
-          reject(buildErrObject(404, 'NOT_FOUND'))
-        }
-        resolve(user)
+    model.findById(id, '+password', (err, user) => {
+      if (err) {
+        reject(buildErrObject(422, err.message))
       }
-    )
+      if (!user) {
+        reject(buildErrObject(404, 'NOT_FOUND'))
+      }
+
+      // Asigns new values
+      for (const field in req.body) {
+        user[field] = req.body[field]
+      }
+
+      // Saves in DB
+      user.save((error, item) => {
+        if (err) {
+          reject(buildErrObject(422, error.message))
+        }
+        // Convert user to object and remove unneeded properties
+        const userObject = item.toObject()
+        delete userObject._id
+        delete userObject.role
+        delete userObject.updatedAt
+        delete userObject.createdAt
+        delete userObject.password
+        resolve(userObject)
+      })
+    })
   })
 }
 
