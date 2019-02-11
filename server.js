d909d170d410228d489655efb282e4c49af7c5ce
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-11 14:46:39 -0500

    server.js now inits redis stuff only in production

--- server.js
+++ server.js
@@ -8,41 +8,48 @@ const cors = require('cors')
 const passport = require('passport')
 const app = express()
 const initMongo = require('./config/mongo')
-const getExpeditiousCache = require('express-expeditious')
-const cache = getExpeditiousCache({
-  namespace: 'expresscache',
-  defaultTtl: '1 minute',
-  engine: require('expeditious-engine-redis')({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
-  })
-})
 
-// Setup express server
+// Setup express server port fron ENV, default: 3000
 app.set('port', process.env.PORT || 3000)
 
+// Enable only in develoment HTTP request logger middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
+
+// Redis cache enabled only for production
+if (process.env.NODE_ENV === 'production') {
+  const getExpeditiousCache = require('express-expeditious')
+  const cache = getExpeditiousCache({
+    namespace: 'expresscache',
+    defaultTtl: '1 minute',
+    engine: require('expeditious-engine-redis')({
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT
+    })
+  })
+  app.use(cache)
+}
+
+// for parsing json
 app.use(
   bodyParser.json({
     limit: '20mb'
   })
 )
+// for parsing application/x-www-form-urlencoded
 app.use(
   bodyParser.urlencoded({
     limit: '20mb',
     extended: true
   })
-) /* for parsing application/x-www-form-urlencoded ~*/
+)
+
+// Init all other stuff
 app.use(cors())
 app.use(passport.initialize())
 app.use(compression())
 app.use(helmet())
-// Redis cache enabled only for production
-if (process.env.NODE_ENV === 'production') {
-  app.use(cache)
-}
 app.use(express.static('public'))
 app.use(require('./app/routes'))
 app.listen(app.get('port'))
