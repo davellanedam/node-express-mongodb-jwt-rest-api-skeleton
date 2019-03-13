6ac3e9f9e762f4b92ff7a7d2f39f9922e3a42fe6
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-13 17:30:24 -0500

    passport JSDoc

--- config/passport.js
+++ config/passport.js
@@ -3,6 +3,11 @@ const User = require('../app/models/user')
 const { decrypt } = require('../app/controllers/utils')
 const JwtStrategy = require('passport-jwt').Strategy
 
+/**
+ * Extracts token from: header, body or query
+ * @param {Object} req - request object
+ * @returns {string} token - decrypted token
+ */
 const jwtExtractor = req => {
   let token = null
   if (req.headers.authorization) {
@@ -18,11 +23,17 @@ const jwtExtractor = req => {
   return token
 }
 
+/**
+ * Options object for jwt middlware
+ */
 const jwtOptions = {
   jwtFromRequest: jwtExtractor,
   secretOrKey: process.env.JWT_SECRET
 }
 
+/**
+ * Login with JWT middleware
+ */
 const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
   User.findById(payload._id, (err, user) => {
     if (err) {
