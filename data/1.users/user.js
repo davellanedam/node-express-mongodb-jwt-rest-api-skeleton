1868e3a6efe58c4faed566180d3039ef102811be
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-13 15:12:02 -0500

    added verification for default admin user in seeding

--- data/1.users/user.js
+++ data/1.users/user.js
@@ -9,6 +9,7 @@ module.exports = [
     password: '$2a$05$2KOSBnbb0r.0TmMrvefbluTOB735rF/KRZb4pmda4PdvU9iDvUB26',
     role: 'admin',
     verified: true,
+    verification: '3d6e072c-0eaf-4239-bb5e-495e6486148f',
     city: 'Bucaramanga',
     country: 'Colombia',
     phone: '123123',
