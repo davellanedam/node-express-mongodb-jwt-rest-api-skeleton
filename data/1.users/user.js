4a5bba0ca3a015082ff8fbe7e53965819e53c65b
Cyriaque <cyriaquedelaunay@gmail.com>
2020-02-18 19:49:31 +0100

    Added a user in seed

--- data/1.users/user.js
+++ data/1.users/user.js
@@ -17,5 +17,21 @@ module.exports = [
     urlGitHub: faker.internet.url(),
     createdAt: faker.date.past(),
     updatedAt: faker.date.recent()
+  },
+  {
+    _id: new ObjectID('5aa1c2c35ef7a4e97b5e995b'),
+    name: 'Simple user',
+    email: 'user@user.com',
+    password: '$2a$05$2KOSBnbb0r.0TmMrvefbluTOB735rF/KRZb4pmda4PdvU9iDvUB26',
+    role: 'user',
+    verified: true,
+    verification: '3d6e072c-0eaf-4239-bb5e-495e6486148d',
+    city: 'Bucaramanga',
+    country: 'Colombia',
+    phone: '123123',
+    urlTwitter: faker.internet.url(),
+    urlGitHub: faker.internet.url(),
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.recent()
   }
 ]
