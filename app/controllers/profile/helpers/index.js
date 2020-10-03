788b025b3674747571f6d5752e2c0456ada65156
Daniel Avellaneda <davellanedam@gmail.com>
2020-10-03 01:02:15 -0400

    Remove unneeded function

--- app/controllers/profile/helpers/index.js
+++ app/controllers/profile/helpers/index.js
@@ -1,13 +1,11 @@
 const { changePasswordInDB } = require('./changePasswordInDB')
 const { findUser } = require('./findUser')
 const { getProfileFromDB } = require('./getProfileFromDB')
-const { passwordsDoNotMatch } = require('./passwordsDoNotMatch')
 const { updateProfileInDB } = require('./updateProfileInDB')
 
 module.exports = {
   changePasswordInDB,
   findUser,
   getProfileFromDB,
-  passwordsDoNotMatch,
   updateProfileInDB
 }
